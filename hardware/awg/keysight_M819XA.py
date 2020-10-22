66d5f397c538b77f4f5bfef71f191c883169b252
Timo <timo.joas@gmx.de>
2020-10-22 14:18:06 +0200

    minor changes

--- hardware/awg/keysight_M819XA.py
+++ hardware/awg/keysight_M819XA.py
@@ -1998,7 +1998,7 @@ class AWGM8195A(AWGM819X):
     _modtype = 'hardware'
 
     awg_mode_cfg = ConfigOption(name='awg_mode', default='MARK', missing='warn')
-    sample_rate_div = ConfigOption(name='sample_rate_div', default=4, missing='warn')
+    sample_rate_div = ConfigOption(name='sample_rate_div', default=4, missing='warn')  # todo: _sample_rate_div also in base
 
     _wave_mem_mode = 'awg_segments'
     wave_file_extension = '.bin8'

1b174f7beeb70752cae0395fc1a4fd06026aaba3
kiotex <kajiyama.k.aa@m.titech.ac.jp>
2021-04-07 22:31:56 +0900

    ヘッダファイルのパーサーの完成

--- hardware/diy_daq/cpp_header_parser.py
+++ hardware/diy_daq/cpp_header_parser.py
@@ -8,29 +8,35 @@ def read_header(header_filepath):
     for l in lst:
         items = l.split()
 
-# 複数行コメントの除去
+        # 複数行コメントの除去
         if len(items) != 0 and items[0] == "/*":
             comment_flg = True
             continue
-        if comment_flg == True:
+        if comment_flg is True:
             if len(items) != 0 and items[0] == "*/":
                 comment_flg = False
             continue
-# 単行コメントの除去
+
+        # 単行コメントの除去
         if len(items) != 0 and ("//" in items[0]):
             continue
 
-# 字書型に格納する
+        # 字書型に格納する
         if len(items) == 3:
             if items[0] == "#define" or items[1] == "=":
                 if "x" in items[2]:
-                    val = int( items[2].replace(',', ''), 16 )
-#                    val = val.to_bytes((val.bit_length() + 7) // 8, byteorder='big')
+                    val = int(items[2].replace(',', ''), 16)
+                    #val = val.to_bytes((val.bit_length() + 7) // 8, byteorder='big')
                 else:
-                    val = int( items[2].replace(',', '') )
+                    val = float(items[2].replace(',', ''))
 
                 if items[0] == "#define":
-            	    ret[ items[1] ] = val
+                    ret[items[1]] = val
                 elif items[1] == "=":
-            	    ret[ items[0] ] = val
+                    ret[items[0]] = val
     return ret
+
+
+if __name__ == "__main__":
+    ret = read_header(r"hardware\diy_daq\MAX11300Hex_20210406.h")
+    ret

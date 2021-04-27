91a3bceaf096d829a2bb6bf8f1f34bdaba697c5f
kiotex <kajiyama.k.aa@m.titech.ac.jp>
2021-04-27 20:18:45 +0900

    diy_daqのクラス作成と自動化のテスト

--- hardware/diy_daq/MAX113XX.py
+++ hardware/diy_daq/MAX113XX.py
@@ -3,130 +3,135 @@
 from pyftdi.spi import SpiController
 from hardware.diy_daq import cpp_header_parser
 
-configDesignVals = ["gpo_data_15_to_0_DESIGNVALUE",
-                    "gpo_data_19_to_16_DESIGNVALUE",
-                    0,  # reserved
-                    "device_control_DESIGNVALUE",
-                    "interrupt_mask_DESIGNVALUE",
-                    "gpi_irqmode_7_to_0_DESIGNVALUE",
-                    "gpi_irqmode_15_to_8_DESIGNVALUE",
-                    "gpi_irqmode_19_to_16_DESIGNVALUE",
-                    0,  # reserved
-                    "dac_preset_data_1_DESIGNVALUE",
-                    "dac_preset_data_2_DESIGNVALUE",
-                    "tmp_mon_cfg_DESIGNVALUE",
-                    "tmp_mon_int_hi_thresh_DESIGNVALUE",
-                    "tmp_mon_int_lo_thresh_DESIGNVALUE",
-                    "tmp_mon_ext1_hi_thresh_DESIGNVALUE",
-                    "tmp_mon_ext1_lo_thresh_DESIGNVALUE",
-                    "tmp_mon_ext2_hi_thresh_DESIGNVALUE",
-                    "tmp_mon_ext2_lo_thresh_DESIGNVALUE"]
 
-portConfigDesignVals = [f'port_cfg_{i:02}_DESIGNVALUE' for i in range(20)]
-dacDesignVals = [f'dac_data_port_{i:02}_DESIGNVALUE' for i in range(20)]
+class MAX113XX:
+    configDesignVals = ["gpo_data_15_to_0_DESIGNVALUE",
+                        "gpo_data_19_to_16_DESIGNVALUE",
+                        0,  # reserved
+                        "device_control_DESIGNVALUE",
+                        "interrupt_mask_DESIGNVALUE",
+                        "gpi_irqmode_7_to_0_DESIGNVALUE",
+                        "gpi_irqmode_15_to_8_DESIGNVALUE",
+                        "gpi_irqmode_19_to_16_DESIGNVALUE",
+                        0,  # reserved
+                        "dac_preset_data_1_DESIGNVALUE",
+                        "dac_preset_data_2_DESIGNVALUE",
+                        "tmp_mon_cfg_DESIGNVALUE",
+                        "tmp_mon_int_hi_thresh_DESIGNVALUE",
+                        "tmp_mon_int_lo_thresh_DESIGNVALUE",
+                        "tmp_mon_ext1_hi_thresh_DESIGNVALUE",
+                        "tmp_mon_ext1_lo_thresh_DESIGNVALUE",
+                        "tmp_mon_ext2_hi_thresh_DESIGNVALUE",
+                        "tmp_mon_ext2_lo_thresh_DESIGNVALUE"]
 
+    portConfigDesignVals = [f'port_cfg_{i:02}_DESIGNVALUE' for i in range(20)]
+    dacDesignVals = [f'dac_data_port_{i:02}_DESIGNVALUE' for i in range(20)]
 
-def setConfigure(ftdi_interface, ccp_header_file):
-    spi = SpiController()
-    spi.configure(ftdi_interface)
-    global slave
-    slave = spi.get_port(cs=0, freq=12E6, mode=0)
+    def __init__(self, ftdi_interface, ccp_header_file):
+        self.ftdi_interface = ftdi_interface
+        self.ccp_header_file = ccp_header_file
 
-    global ccp_header
-    ccp_header = cpp_header_parser.read_header(ccp_header_file)
+        self.setConfigure()
 
-    def makeup_data(keylist):
-        return [ReadFromCppHeader(key) for key in keylist]
+    def setConfigure(self):
+        self.spi = SpiController()
+        self.spi.configure(self.ftdi_interface)
 
-    blockWrite(ccp_header["gpo_data_15_to_0"], makeup_data(configDesignVals))
-    blockWrite(ccp_header["port_cfg_00"], makeup_data(portConfigDesignVals))
-    blockWrite(ccp_header["dac_data_port_00"], makeup_data(dacDesignVals))
+        self.slave = self.spi.get_port(cs=0, freq=12E6, mode=0)
 
+        self.ccp_header = cpp_header_parser.read_header(self.ccp_header_file)
 
-def ReadFromCppHeader(val):
-    try:
-        if val in ccp_header.keys():
-            return ccp_header[val]
-        else:
+        self.blockWrite(
+            self.ccp_header["gpo_data_15_to_0"],
+            self.makeup_data(
+                self.configDesignVals))
+        self.blockWrite(
+            self.ccp_header["port_cfg_00"],
+            self.makeup_data(
+                self.portConfigDesignVals))
+        self.blockWrite(
+            self.ccp_header["dac_data_port_00"],
+            self.makeup_data(
+                self.dacDesignVals))
+
+    def makeup_data(self, keylist):
+        return [self.ReadFromCppHeader(key) for key in keylist]
+
+    def ReadFromCppHeader(self, val):
+        try:
+            if val in self.ccp_header.keys():
+                return self.ccp_header[val]
+            else:
+                return val
+        except NameError:
+            print("You should execute setConfigure()")
             return val
-    except NameError:
-        print("You should execute setConfigure()")
-        return val
 
+    def MAX113XXAddr_SPI_Write(self, reg):
+        """
+        SPI first byte when writing MAX11300/11
+        (7-bit address in bits 0x7E; LSB=0 for write)
+        """
+        RegAddr = self.ReadFromCppHeader(reg)
+        return (RegAddr << 1)
 
-def MAX113XXAddr_SPI_Write(reg):
-    """
-    SPI first byte when writing MAX11300/11
-    (7-bit address in bits 0x7E; LSB=0 for write)
-    """
-    RegAddr = ReadFromCppHeader(reg)
-    return (RegAddr << 1)
+    def MAX113XXAddr_SPI_Read(self, reg):
+        """
+        SPI first byte when reading MAX11300/11
+        (7-bit address in bits 0x7E; LSB=1 for read)
+        """
+        RegAddr = self.ReadFromCppHeader(reg)
+        return ((RegAddr << 1) | 1)
 
+    def writeRegister(self, reg, data):
+        tx_reg = self.MAX113XXAddr_SPI_Write(reg)
+        tx_buf = [tx_reg, ((0xFF00 & data) >> 8), (0x00FF & data)]
 
-def MAX113XXAddr_SPI_Read(reg):
-    """
-    SPI first byte when reading MAX11300/11
-    (7-bit address in bits 0x7E; LSB=1 for read)
-    """
-    RegAddr = ReadFromCppHeader(reg)
-    return ((RegAddr << 1) | 1)
+        self.slave.exchange(tx_buf, readlen=0, duplex=True)
 
+    def readRegister(self, reg):
+        tx_reg = self.MAX113XXAddr_SPI_Read(reg)
+        tx_buf = [tx_reg, 0x00, 0x00]
+        # 0x0000 is dummy data.
 
-def writeRegister(reg, data):
-    tx_reg = MAX113XXAddr_SPI_Write(reg)
-    tx_buf = [tx_reg, ((0xFF00 & data) >> 8), (0x00FF & data)]
+        rx_buf = self.slave.exchange(tx_buf, readlen=3, duplex=True)
 
-    rx_buf = slave.exchange(tx_buf, readlen=3, duplex=True)
-    # STM32では、tx_reg送ってからdata送る必要があったから、もしかすると分割する必要があるかもしれない。
+        rtn_val = (rx_buf[1] << 8) | rx_buf[2]
+        # rx_buf is [0xff, MSB data, LSB data].
+        return rtn_val
 
+    def blockWrite(self, reg, data):
+        tx_reg = self.MAX113XXAddr_SPI_Write(reg)
+        tx_buf = [tx_reg]
 
-def readRegister(reg):
-    tx_reg = MAX113XXAddr_SPI_Read(reg)
-    tx_buf = [tx_reg, 0x00, 0x00]
-    # 0x0000 is dummy data.
+        for data_buf in data:
+            tx_buf.append((0xFF00 & data_buf) >> 8)
+            tx_buf.append((0x00FF & data_buf))
 
-    rx_buf = slave.exchange(tx_buf, readlen=3, duplex=True)
-    # STM32では、tx_reg送ってからdata送る必要があったから、もしかすると分割する必要があるかもしれない。
-    rtn_val = (rx_buf[1] << 8) | rx_buf[2]
-    # rx_buf is [0xff, MSB data, LSB data].
-    return rtn_val
+        self.slave.exchange(tx_buf, readlen=0, duplex=True)
 
+    def blockRead(self, reg, num_reg):
+        tx_reg = self.MAX113XXAddr_SPI_Read(reg)
+        tx_buf = [tx_reg] + [0x00] * (2 * num_reg)
 
-def blockWrite(reg, data):
-    tx_reg = MAX113XXAddr_SPI_Write(reg)
-    tx_buf = [tx_reg]
+        rx_buf = self.slave.exchange(tx_buf, readlen=(1 + 2 * num_reg), duplex=True)
 
-    for data_buf in data:
-        tx_buf.append((0xFF00 & data_buf) >> 8)
-        tx_buf.append((0x00FF & data_buf))
+        rtn_data = []
+        for idx in range(num_reg):
+            rtn_data.append((rx_buf[2 * idx + 1] << 8) | rx_buf[2 * idx + 2])
 
-    slave.exchange(tx_buf, readlen=0, duplex=True)
+        return rtn_data
 
-
-def blockRead(reg, num_reg):
-    tx_reg = MAX113XXAddr_SPI_Read(reg)
-    tx_buf = [tx_reg] + [0x00] * (2 * num_reg)
-
-    rx_buf = slave.exchange(tx_buf, readlen=(1 + 2 * num_reg), duplex=True)
-    # STM32では、tx_reg送ってからdata送る必要があったから、もしかすると分割する必要があるかもしれない。
-
-    rtn_data = []
-    for idx in range(num_reg):
-        rtn_data.append((rx_buf[2 * idx + 1] << 8) | rx_buf[2 * idx + 2])
-
-    return rtn_data
-
-
-def dumpMemory():
-    mem = blockRead(0x00, 0x74)
-    for idx, data in enumerate(mem):
-        print('Register 0x{:02X} = 0x{:04X}'.format(idx, data))
+    def dumpMemory(self):
+        mem = self.blockRead(0x00, 0x74)
+        for idx, data in enumerate(mem):
+            print('Register 0x{:02X} = 0x{:04X}'.format(idx, data))
 
 
 if __name__ == "__main__":
     from pyftdi.ftdi import Ftdi
     Ftdi.show_devices()
 
-    setConfigure('ftdi://ftdi:2232:0:1/1',
+    max11300 = MAX113XX('ftdi://ftdi:2232:0:1/1',
                  r"hardware\diy_daq\MAX11300Hex.h")
-    dumpMemory()
+    max11300.dumpMemory()
---
+++ hardware/diy_daq/diy_daq.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Arios ATC-204(東邦電子 TTM-204)制御用コード
+
+Qudi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Qudi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
+top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
+"""
+
+from core.module import Base
+from core.connector import Connector
+from core.configoption import ConfigOption
+from interface.process_interface import ProcessInterface
+from interface.process_control_interface import ProcessControlInterface
+
+from pyftdi.ftdi import Ftdi
+from hardware.diy_daq.MAX113XX import MAX113XX
+
+import serial
+import time
+
+
+class DIY_DAQ(Base, ProcessInterface, ProcessControlInterface):
+    """ Methods to control Arios ATC-204(東邦電子 TTM-204).
+    https://dia-pe-titech.esa.io/posts/158
+
+    Example config for copy-paste:
+
+    DIY_DAQ:
+        module.Class: 'diy_daq.diy_daq.DIY_DAQ'
+        port: 'COM4'
+        ftdi_interface: "ftdi:///1"
+        ccp_header_file: r"hardware\diy_daq\MAX11300Hex.h"
+
+        limit_min: 0    # Pa
+        limit_max: 40e3 # Pa
+    """
+
+    _ftdi_interface = ConfigOption(
+        'ftdi_interface', "ftdi:///1", missing='warn')
+    _ccp_header_file = ConfigOption(
+        'ccp_header_file',
+        r"hardware\diy_daq\MAX11300Hex.h",
+        missing='warn')
+
+    def on_activate(self):
+        """ Activate module.
+        """
+        Ftdi.show_devices()
+
+        self.max113xx = MAX113XX(self._ftdi_interface,
+                                 self._ccp_header_file)
+
+    def on_deactivate(self):
+        """ Deactivate module.
+        """
+
+    def get_process_unit(self, channel=None):
+        """ Process unit, here Pa.
+
+            @return float: process unit
+        """
+        return 'V', 'volts'
+
+    def get_control_unit(self, channel=None):
+        """ Get unit of control value.
+
+            @return tuple(str): short and text unit of control value
+        """
+        return 'V', 'volts'
+
+    def get_control_limit(self, channel=None):
+        """ Get minimum and maximum of control value.
+
+            @return tuple(float, float): minimum and maximum of control value
+        """
+        # FIXME:
+        return 0, 10
+
+    def get_minimal_step(self):
+        return 1 / self.bin_to_volt(0x001)
+
+    def bin_to_volt(self, val):
+        return val / 0xfff * 10
+
+    def volt_to_bin(self, val):
+        return int(val / 10 * 0xfff)
+
+    def get_process_value(self, channel=None):
+        """ Process value, here temperature.
+
+            @return float: process value
+        """
+        if channel is not None:
+            ret = self.max113xx.readRegister(f'adc_data_port_{channel:02}')
+            return self.bin_to_volt(ret)
+        else:
+            ret = self.max113xx.blockRead(f'adc_data_port_00', 20)
+            return [self.bin_to_volt(val) for val in ret]
+
+    def get_control_value(self, channel=None):
+        """ Get current control value, here heating power
+
+            @return float: current control value
+        """
+        if channel is not None:
+            ret = self.max113xx.readRegister(f'dac_data_port_{channel:02}')
+            return self.bin_to_volt(ret)
+        else:
+            ret = self.max113xx.blockRead(f'dac_data_port_00', 20)
+            return list(map(self.bin_to_volt, ret))
+
+    def set_control_value(self, value, channel=None):
+        """ Set control value, here heating power.
+
+            @param float value: control value
+        """
+        if channel is not None:
+            val = self.volt_to_bin(value)
+            self.max113xx.writeRegister(f'dac_data_port_{channel:02}', val)
+        else:
+            val = list(map(self.volt_to_bin, value))
+            ret = self.max113xx.blockWrite(f'dac_data_port_00', val)
